598728e8f0d9917413295f846c5fb2cd374d2f0d
Jordan Gross <grossjordan07@gmail.com>
2026-02-24 22:55:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a simple React Website that allow users to increase and decrease count of a single number.
+
+There is also reset capability. 
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from "./Counter";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Count Master</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
